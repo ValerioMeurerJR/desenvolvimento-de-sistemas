dfae91b7858b2c491355cb0936d38f5dea2d9356
Valerio Meurer <valerio.meurer_jt@live.com>
2025-02-06 19:14:57 -0300

    SEMANA 01

--- semana01/src/exercicio03.ts
+++ semana01/src/exercicio03.ts
@@ -1,12 +1,19 @@
+import { differenceInDays } from "date-fns";
 import leia from "readline-sync"
 
 export default function exercicio03(){
-    var diaNascimento = leia.questionInt("Digite a dia nascimento: ");
-    let mesNascimento = leia.questionInt("Digite a Mes: ");
-    let anoNascimento = leia.questionInt("Digite a Ano: ");
-    let data = new Date();
-    let dias = ((data.getMonth() + 1) * 30) + (data.getFullYear() * 365) + data.getDate()
-    let diasNascimento = (mesNascimento * 30) + (anoNascimento * 365) + diaNascimento
-    let diastotal = dias - diasNascimento 
-    console.log(diastotal)
+    // var diaNascimento = leia.questionInt("Digite a dia nascimento: ");
+    // let mesNascimento = leia.questionInt("Digite a Mes: ");
+    // let anoNascimento = leia.questionInt("Digite a Ano: ");
+    // let data = new Date();
+    // let dias = ((data.getMonth() + 1) * 30) + (data.getFullYear() * 365) + data.getDate()
+    // let diasNascimento = (mesNascimento * 30) + (anoNascimento * 365) + diaNascimento
+    // let diastotal = dias - diasNascimento 
+    // console.log(diastotal)
+    
+    var dataNascimento = leia.questionInt("Digite a data de nascimento: ");
+    const dateFormat = new Date(dataNascimento)
+    const date = new Date()
+    const dias = differenceInDays(date, dateFormat)
+    console.log(`NUMERO DE DIAS: ${dias}`)
 }
--- semana01/src/exercicio06.ts
+++ semana01/src/exercicio06.ts
@@ -7,8 +7,8 @@ export default function exercicio06(){
     }   
     let desconto = valor * 0.25
     let valorArrecadado = valor - desconto
-    console.log(`VALOR ARRECADADO ${valor}`)
-    console.log(`VALOR DE DESCONTO 25% ${desconto}`)
-    console.log(`VALOR ARRECADADO ${valorArrecadado}`)
+    console.log(`VALOR DAS CORRIDAS: R$ ${valor.toFixed(0)}`)
+    console.log(`VALOR DE DESCONTO 25%: R$ ${desconto.toFixed(0)}`)
+    console.log(`VALOR ARRECADADO PELO MOTORISTA: R$ ${valorArrecadado.toFixed(0)}`)
 
 }
